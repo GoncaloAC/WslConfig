aaab873096d852db7ec55ae807e03638f7acff7b
GoncaloAC <48471870+GoncaloAC@users.noreply.github.com>
2023-09-09 21:00:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,11 +21,11 @@ info() {
 
 info "Updating Ubuntu in"
 
-sudo -i apt-get update > /dev/null 2>&1
-sudo apt-get upgrade -y > /dev/null 2>&1
+sudo -i apt-get update
+sudo apt-get upgrade -y
 
 timer "Installing JDK 17 in"
-echo "sudo apt-get install openjdk-17-jdk -y"
+sudo apt-get install openjdk-17-jdk -y
 echo >> ~/.bashrc
 echo 'export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64' >> ~/.bashrc
 
@@ -51,4 +51,41 @@ bash Miniconda3-py39_4.12.0-Linux-x86_64.sh
 rm Miniconda3-py39_4.12.0-Linux-x86_64.sh
 
 timer "Installing MariaDB in"
-sudo apt install mariadb-server
+sudo apt install mariadb-server -y
+
+timer "Installing Apache Cassandra in"
+sudo apt install openjdk-8-jre -y
+sudo apt install apt-transport-https gnupg2 -y
+sudo wget -q -O - https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+sudo sh -c 'echo "deb http://www.apache.org/dist/cassandra/debian 311x main" > /etc/apt/sources.list.d/cassandra.list'
+sudo apt update
+sudo apt install cassandra -y
+echo 'JAVA_HOME=usr/lib/jvm/java-8-openjdk-amd64' >> ~/usr/share/cassandra/cassandra.in.sh
+                    
+timer "Installing Redis in"
+sudo apt install redis-server -y
+
+timer "Installing Neo4j in"
+wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb https://debian.neo4j.com stable latest' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
+sudo apt-get update
+sudo apt-get install neo4j-enterprise -y
+
+timer "Installing Google Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install --fix-broken -y
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+timer "Installing Mozzila Firefox"
+sudo add-apt-repository ppa:mozillateam/ppa
+sudo apt install firefox
+
+timer "Installing Microsoft Edge"
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list'
+sudo rm microsoft.gpg
+sudo apt update
+sudo apt install microsoft-edge-beta
