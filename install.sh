48c7df50408a11afb1f2161b8e6eed5b42ab3595
GoncaloAC <48471870+GoncaloAC@users.noreply.github.com>
2023-09-10 00:24:50 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -104,3 +104,32 @@ sudo apt install microsoft-edge-beta
 timer "Microsoft Edge installed successfully!"
 
 timer "Installing the scripts now..."
+
+mkdir -p ~/.scripts/
+cp ./install.sh ~/.scripts/install.sh
+cp ./config.sh ~/.scripts/config.sh
+cp ./start.sh ~/.scripts/start.sh
+cp ./stop.sh ~/.scripts/stop.sh
+
+success "Scripts copied successfully!"
+
+chmod u+x ~/.scripts/install.sh
+chmod u+x ~/.scripts/config.sh
+chmod u+x ~/.scripts/start.sh
+chmod u+x ~/.scripts/stop.sh
+
+sed -i 's/\r$//' ~/.scripts/install.sh
+sed -i 's/\r$//' ~/.scripts/config.sh
+sed -i 's/\r$//' ~/.scripts/start.sh
+sed -i 's/\r$//' ~/.scripts/stop.sh
+
+success "Executable permissions assigned to scripts!"
+
+echo >>  ~/.bashrc
+echo "alias install='~/.scripts/install.sh'" >> ~/.bashrc
+echo "alias config='~/.scripts/config.sh'" >> ~/.bashrc
+echo "alias start='~/.scripts/start.sh'" >> ~/.bashrc
+echo "alias stop='~/.scripts/stop.sh'" >> ~/.bashrc
+source ~/.bashrc
+
+success "Scripts successfully configured!"
